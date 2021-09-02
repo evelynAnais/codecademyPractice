282a7b36ebbcb8683cad4bbf0f3731ffadd7198a
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-09-02 15:19:24 -0600

    initial commit

---
+++ Kelvin Weather.js
@@ -0,0 +1,10 @@
+// kelvin is 239
+const kelvin = 0
+// subtracting from kelvin to get celsius
+const celsius = kelvin - 273
+//converting cel to fahere
+let fahrenheit = celsius*(9/5)+32
+//rounding
+fahrenheit = Math.floor(fahrenheit)
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
