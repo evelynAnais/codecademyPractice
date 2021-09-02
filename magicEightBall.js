ee42c584fb9d1fb39ca1a232940ea55171a5d2a6
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-09-02 15:22:45 -0600

    addedMagic eightball

---
+++ magicEightBall.js
@@ -0,0 +1,42 @@
+let userName = 'Daric'
+userName === '' ? console.log('Hello') : console.log(`Hello, ${userName}`)
+
+let userQuestion = 'Will evelyn make me food'
+
+console.log(`${userName} asked... ${userQuestion}?`)
+
+let randomNumber = Math.floor(Math.random()*8)
+
+let eightBall = ''
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain'
+    break;
+  case 1:
+    eightBall = 'It is decidedly so'
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+  case 3:
+    eightBall = 'Cannot predict now'
+    break;
+  case 4:
+    eightBall = 'Do not count on it'
+    break;
+  case 5:
+    eightBall = 'My sources say no'
+    break;
+  case 6:
+    eightBall = 'Outlook not so good'
+    break;
+  case 7:
+    eightBall = 'Signs point to yes'
+    break;
+  default:
+    eightBall = 'HELLO'
+    break;
+}
+
+console.log(`The eightball answered ${eightBall}`)
