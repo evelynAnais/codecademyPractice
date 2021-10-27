af6c6005926d623f5206d9a55f2b414144f2cd06
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-10-27 12:55:03 -0600

    Mr.Boo

---
+++ learningCS/consoleCreatures.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //beginning of face
+      Console.WriteLine(" .-.");   
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Mr.Boo");
+    }
+  }
+}
